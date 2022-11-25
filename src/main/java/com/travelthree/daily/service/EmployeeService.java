34866f7efc240b2f827226a6c51bb3091d05e78e
Faust <1546327522@qq.com>
2022-11-25 14:05:14 +0800

    feature: update login filter

--- src/main/java/com/travelthree/daily/service/EmployeeService.java
+++ src/main/java/com/travelthree/daily/service/EmployeeService.java
@@ -22,4 +22,6 @@ public interface EmployeeService {
      * @return 用户id
      */
     EmployeeDTO login(String username, String password);
+
+    EmployeeDTO getEmployeeById(String uid);
 }
---
+++ src/main/java/com/travelthree/daily/utils/TaleUtil.java
@@ -0,0 +1,37 @@
+package com.travelthree.daily.utils;
+
+import com.travelthree.daily.constant.WebConstant;
+import com.travelthree.daily.dto.EmployeeDTO;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author 陈宣辰
+ * @date 2022-11-23 18:00:24
+ * @description
+ */
+public class TaleUtil {
+
+    /**
+     * 获取当前登录用户
+     * */
+    public static EmployeeDTO getCurrentLoginUser(HttpServletRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return (EmployeeDTO) request.getSession().getAttribute(WebConstant.LOGIN_SESSION_KEY);
+    }
+
+    /**
+     * 从Cookie中获取Uid
+     * */
+    public static String getUidFromCookie(HttpServletRequest request) {
+        for (Cookie cookie : request.getCookies()) {
+            if (cookie.getName().equals(WebConstant.REMEMBER_COOKIE_KEY)) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+}
