c6165682c6bc37dfd1f6c6374ad60567dd00cf0d
Faust <1546327522@qq.com>
2022-12-17 01:32:15 +0800

    feature：check leave

--- src/main/java/com/travelthree/daily/dto/LeaveStatusParam.java
+++ src/main/java/com/travelthree/daily/dto/LeaveStatusParam.java
@@ -4,9 +4,10 @@ import com.travelthree.daily.constant.LeaveCheckStatus;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class LeaveStatusParam {
-    @NotBlank
+    @NotNull
     private LeaveCheckStatus status;
 }
